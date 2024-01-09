bceffb5af1b3c3889022832efdca61dbb177f5bc
nurbekasan <155700273+nurbekasan@users.noreply.github.com>
2024-01-09 14:50:51 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+const chatbox = document.querySelector(".chatbox");
+
+let userMessage;
+const API_KEY = "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";
+
+
+const createChatLi = (message, className) => {
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", className);
+    let chatContent = className === "outgoing" ? `<p>${message}</p>` : `                <span class="material-symbols-outlined">smart_toy</span>
+    <p>${message}</p>`;
+    chatLi.innerHTML = chatContent;
+    return chatLi
+    
+}
+
+const generateResponce = (incomingChatli) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = incomingChatli.querySelector("p")
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) =>{
+        messageElement.textContent = "Opps! Kate";
+    })
+}
+
+const  handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if(!userMessage) return;
+    chatbox .appendChild(createChatLi(userMessage, "outgoing"));
+
+    setTimeout(() => {
+
+        const incomingChatli = createChatLi("THingking...", "incoming");
+        chatbox.appendChild(incomingChatli);
+        generateResponce(incomingChatli);
+
+    }, 600);
+
+}
+
+sendChatBtn.addEventListener("click", handleChat);
+//https://platform.openai.com/docs/api-reference
